b732d7b225fbf2b71790461ca5038004a2c9abe4
Nizam <ramazanov570633@gmail.com>
2023-11-28 17:09:37 +0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './src/Page/Home/Home'
+
+const App = () => {
+  return (
+    <View>
+      <Home></Home>
+    </View>
+  )
+}
+
+export default App
