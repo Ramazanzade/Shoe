fbd638f3cf3f26dfc7049778d4b0cc38ba02e7a9
Nizam <ramazanov570633@gmail.com>
2023-11-30 08:27:49 +0400

    Failed

--- src/Page/3DModel/3DModel.tsx
+++ src/Page/3DModel/3DModel.tsx
@@ -1,72 +1,30 @@
-// import { View, Text } from 'react-native'
-// import { useRef, useState } from 'react'
-// import { Canvas, useFrame } from '@react-three/fiber'
-// import { OrbitControls } from '@react-three/drei'
-
-// function Box(props:any) {
-//   const ref = useRef<any>()
-//   const [hovered, hover] = useState(false)
-//   const [clicked, click] = useState(false)
-//   useFrame((state, delta) => (ref.current.rotation.x += delta))
-//   return (
-//     <mesh
-//       {...props}
-//       ref={ref}
-//       scale={clicked ? 1.5 : 1}
-//       onClick={(event) => click(!clicked)}
-//       onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-//       onPointerOut={(event) => hover(false)}>
-//       <boxGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//     </mesh>
-//   )
-// }
-
-// const Model = () => {
-//   return (
-//     <Canvas>
-//       <ambientLight intensity={Math.PI / 2} />
-//       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-//       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-//       <Box position={[-1.2, 0, 0]} />
-//       <Box position={[1.2, 0, 0]} />
-//       <OrbitControls />
-//     </Canvas> 
-//   )
-// }
-
-// export default Model
-
-
-
-
-
-
-
-
-import { View } from 'react-native';
-import { Canvas, useFrame } from 'react-three-fiber';
-// import { OBJLoader } from 'three/addons/loaders/OBJLoader';
-import React, { useRef } from 'react';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import ModelView from 'react-native-3d-model-view';
 
 const Model = () => {
-  const modelRef = useRef<any>();
-  useFrame(() => {
-    if (modelRef.current) {
-      modelRef.current.rotation.x += 0.01;
-      modelRef.current.rotation.y += 0.01;
-    }
-  });
-
   return (
-    <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <mesh ref={modelRef} onClick={(e) => console.log('click')} position={[0, 0, 0]}>
-      <primitive object={require('./q.obj')} />
-    </mesh>
-  </Canvas>
-  );
-};
+    <View style={styles.container}>
+    <ModelView
+      source={{
+        model: require('./q.obj'),
+      }}
+      style={styles.model}
+    />
+  </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  model: {
+    width: '100%',
+    height: '100%',
+  },
+});
+export default Model
 
-export default Model;
+
