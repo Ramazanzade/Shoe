b5854da57e0a061d3cf885f20ce6c25785bbed7e
Nizam <ramazanov570633@gmail.com>
2023-12-03 19:36:37 +0400

    error-3

--- src/Page/3DModel/3DModel.tsx
+++ src/Page/3DModel/3DModel.tsx
@@ -1,7 +1,34 @@
-import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { getProject } from '@theatre/core'
+// import * as THREE from 'three'
+// import React, { useRef, useState } from 'react'
+// import { Canvas, useFrame } from '@react-three/fiber'
+// import { getProject } from '@theatre/core'
+// const Model = () => {
+//   return (
+//     <Canvas
+//       camera={{
+//         position: [5, 5, -5],
+//         fov: 75,
+//       }}
+//     >
+//       <ambientLight />
+//       <pointLight position={[10, 10, 10]} />
+//       <mesh>
+//         <boxGeometry args={[1, 1, 1]} />
+//         <meshStandardMaterial color="orange" />
+//       </mesh>
+//     </Canvas>
+//   )
+// }
+
+// export default Model
+
+
+
+
+
+
+import React from 'react'
+import { Canvas} from '@react-three/fiber'
 const Model = () => {
   return (
     <Canvas
